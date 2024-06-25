4cd252a46955c13da1cefb29c613cb2795d10409
Kraken57 <kasaad1609@gmail.com>
2024-06-25 19:18:58 +0530

    Pancake swap implementation

--- AbiInfo.js
+++ AbiInfo.js
@@ -1,3 +1,25 @@
+//application binary interface to interact with contracts
 // Info about to which function to have to communicated with in different 
 // contracts like factory contract, Router contract etc  
 
+const erc20ABI=["function decimals() public view returns (uint8)"] //abi to interact with erc-20 and provide decimal places to your token
+
+//it will return the address of liquidty pool contract
+//0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16 for BUSD and WBNB
+const factoryABI=[
+    "function getPair(address tokenA, address tokenB) external view returns (address pair)"
+]
+
+const pairABI=[
+    "function token0() external view returns (address)",
+    "function token1() external view returns (address)",
+    "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"
+]
+
+
+//router contract used for price switch and all
+const routerABI=[
+    "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)"
+]
+
+module.exports = { erc20ABI, factoryABI, pairABI, routerABI };
--- PriceFetch.js
+++ PriceFetch.js
@@ -1,1 +1,47 @@
 // To fetch price
+
+const ethers = require('ethers');
+const {
+    factoryAddress,
+    routerAddress,
+    fromAddress,
+    toAddress
+} = require("./AddressList")
+
+const {erc20ABI, factoryABI, pairABI, routerABI}= require("./AbiInfo");
+
+//provider to read from the blockchain as we have to read for blockchain
+const provider = new ethers.providers.JsonRpcProvider(
+    "https://bsc-dataseed1.binance.org/"
+)
+
+const factoryInstance = new ethers.Contract(
+    factoryAddress,factoryABI,provider
+)
+// console.log(factoryInstance)
+const routerInstance=new ethers.Contract(
+    routerAddress,routerABI,provider
+)
+
+const priceFetch=async(humanFormat)=>{
+  const token1 = new ethers.Contract(
+    fromAddress,erc20ABI,provider
+  )
+  const token2 = new ethers.Contract(
+    toAddress,erc20ABI,provider
+  )
+  const decimal1= await token1.decimals()//both token have 18 decimals
+  const decimal2= await token2.decimals()
+  const amountIn = ethers.utils.parseUnits(humanFormat,decimal1).toString();//converting the humanFormat to busd that is transfer 
+  const amountsOut = await routerInstance.getAmountsOut(amountIn,[
+    fromAddress,
+    toAddress
+  ])
+  const humanOutput = ethers.utils.formatUnits(
+    amountsOut[1].toString(),
+    decimal2
+  )// convert that toAddress i.e, WBNB to humanOutput
+  console.log("This the number of WBNB: ",humanOutput)
+}
+humanFormat="1000"
+priceFetch(humanFormat)
